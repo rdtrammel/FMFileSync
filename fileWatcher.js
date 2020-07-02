24d0826e344bdcf31009e35b3b6ac18e1f5cc519
rdtrammel <rd.trammel@gmail.com>
2020-07-02 14:03:07 -0500

    changes

--- fileWatcher.js
+++ fileWatcher.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const app = express();
+let app = express();
 const port = 5000;
 
-app.get('/', function (request, response) {
-
-  var params = request; //Gets parameters from the url as a json object
-
-  response.end(params);
+app.get('/', function (req, res) {
+  var params = req.query; //Gets parameters from the url as a json object
+  res.end(params);
 });
 
 app.listen(port, function(){ 
-  console.log("Ready to start watching Files");
+  console.log("Watching Files");
 });
